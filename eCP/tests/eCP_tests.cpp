21d4188c7f06399c5c37e53673e9cd3bdc2e48fc
Morten Skoett <mortenskoett@gmail.com>
2020-10-21 22:10:25 +0200

    Convert ecp_tests to gtest format
    All tests passes.

--- eCP/tests/eCP_tests.cpp
+++ eCP/tests/eCP_tests.cpp
@@ -1,125 +1,107 @@
-#include "pch.h"
+#include <gtest/gtest.h>
 
-// TODO: Fix these tests to use cmake
+#include <eCP/eCP.hpp>
+#include <eCP/pre-processing.hpp>
 
-#include "CppUnitTest.h"
-#include "vector"
-#include "../eCP/eCP.hpp"
+/* Helpers */
 
-using namespace Microsoft::VisualStudio::CppUnitTestFramework;
-using namespace std;
+Index* get_index(unsigned int L = 2) {
+    std::vector<std::vector<float>> descriptors =
+    {
+        {59, 59, 59},
+        {50, 50, 50},
+        {2, 2, 2},
+        {6, 6, 6},
+        {15, 15, 15},
+        {11, 11, 11},
+        {31, 31, 31},
+        {21, 21, 21},
+        {42, 42, 24},
+        {24, 24, 24},
+        {10, 10, 10},
+        {34, 34, 34},
+        {25, 25, 25},
+        {29, 29, 29},
+        {32, 32, 32},
+        {40, 40, 40},
+        {7, 7, 7},
+    };
 
-namespace eCPTests
-{
-	TEST_CLASS(eCPTests)
-	{
-	private:
-		Index* get_index(unsigned int L = 2) {
-			std::vector<std::vector<float>> descriptors =
-			{
-				{59, 59, 59},
-				{50, 50, 50},
-				{2, 2, 2},
-				{6, 6, 6},
-				{15, 15, 15},
-				{11, 11, 11},
-				{31, 31, 31},
-				{21, 21, 21},
-				{42, 42, 24},
-				{24, 24, 24},
-				{10, 10, 10},
-				{34, 34, 34},
-				{25, 25, 25},
-				{29, 29, 29},
-				{32, 32, 32},
-				{40, 40, 40},
-				{7, 7, 7},
-			};
+    return eCP_Index(descriptors, L, 0);
+}
 
-			return eCP_Index(descriptors, L, 0);
-		}
+/* Data structure tests */
+TEST(ecp_tests, point_comparison_given_different_points_returns_false) {
+    EXPECT_FALSE(Point(new float[3]{ 1, 1, 1 }, 1) == Point(new float[3]{ 2, 2, 2 }, 2));
+}
 
-	public:
-#pragma region point
-		TEST_METHOD(point_comparison_given_different_points_returns_false) {
-			Assert::IsFalse(Point(new float[3]{ 1, 1, 1 }, 1) == Point(new float[3]{ 2, 2, 2 }, 2));
-		}
+TEST(ecp_tests, point_comparison_given_same_points_returns_true) {
+    EXPECT_TRUE(Point(new float[3]{ 1, 1, 1 }, 1) == Point(new float[3]{ 1, 1, 1 }, 1));
+}
 
-		TEST_METHOD(point_comparison_given_same_points_returns_true) {
-			Assert::IsTrue(Point(new float[3]{ 1, 1, 1 }, 1) == Point(new float[3]{ 1, 1, 1 }, 1));
-		}
-#pragma endregion
+TEST(ecp_tests, cluster_comparison_given_different_clusters_returns_false) {
+    EXPECT_FALSE(Node(Point(new float[3]{ 1, 1, 1 }, 0)) == Node(Point(new float[3]{ 2, 2, 2 }, 0)));
+}
 
-#pragma region node
+TEST(ecp_tests, cluster_comparison_given_same_clusters_returns_true) {
+    EXPECT_TRUE(Node(Point(new float[3]{ 1, 1, 1 }, 0)) == Node(Point(new float[3]{ 1, 1, 1 }, 0)));
+}
 
-		TEST_METHOD(cluster_comparison_given_different_clusters_returns_false) {
-			Assert::IsFalse(Node(Point(new float[3]{ 1, 1, 1 }, 0)) == Node(Point(new float[3]{ 2, 2, 2 }, 0)));
-		}
+/* Tests */
 
-		TEST_METHOD(cluster_comparison_given_same_clusters_returns_true) {
-			Assert::IsTrue(Node(Point(new float[3]{ 1, 1, 1 }, 0)) == Node(Point(new float[3]{ 1, 1, 1 }, 0)));
-		}
+TEST(ecp_tests, index_given_descriptors_has_correct_depth) {
+    std::vector<std::vector<float>> descriptors = {
+        {1, 1, 1},
+        {2, 2, 2}
+    };
 
-#pragma endregion
+    unsigned int L = 2;
 
-#pragma region fit
+    Index index = *eCP_Index(descriptors, L, 0);
 
-		TEST_METHOD(fit_given_descriptors_has_correct_depth) {
-			std::vector<std::vector<float>> descriptors = {
-				{1, 1, 1},
-				{2, 2, 2}
-			};
-			unsigned int L = 2;
+    EXPECT_TRUE(index.L = L);
 
-			Index index = *eCP_Index(descriptors, L, 0);
+    //check that the deepest level is 2
+    EXPECT_TRUE(index.top_level[0]->children[0]->children.size() == 0);
+    EXPECT_TRUE(index.top_level[0]->children[0]->points.size() != 0);
+}
 
-			Assert::IsTrue(index.L = L);
-			//check that the deepest level is 2
-			Assert::IsTrue(index.top_level[0]->children[0]->children.size() == 0);
-			Assert::IsTrue(index.top_level[0]->children[0]->points.size() != 0);
-		}
+TEST(ecp_tests, index_given_descriptors_inserts_descriptors) {
+    std::vector<std::vector<float>> descriptors = {
+        {1, 1, 1},
+        {2, 2, 2},
+        {3, 3, 3},
+    };
 
-		TEST_METHOD(fit_given_descriptors_inserts_descriptors) {
-			std::vector<std::vector<float>> descriptors = {
-				{1, 1, 1},
-				{2, 2, 2},
-				{3, 3, 3},
-			};
-			unsigned int L = 1;
+    unsigned int L = 1;
 
-			Index index = *eCP_Index(descriptors, L, 0);
+    Index index = *eCP_Index(descriptors, L, 0);
 
-			Assert::IsTrue(index.dataset.size() == descriptors.size());
-		}
-#pragma endregion
+    EXPECT_TRUE(index.dataset.size() == descriptors.size());
+}
 
-#pragma region query
-		TEST_METHOD(query_given_k_1_returns_query_point) {
-			Index* index = get_index();
-			std::vector<float> q = { 5, 5, 5 };
-			unsigned int k = 1;
-			unsigned int b = 1;
+TEST(ecp_tests, query_given_k_1_returns_query_point) {
+    Index* index = get_index();
+    std::vector<float> q = { 5, 5, 5 };
+    unsigned int k = 1;
+    unsigned int b = 1;
 
-			auto actual = query(index, q, k, b);
+    auto actual = query(index, q, k, b);
 
-			Assert::IsTrue(actual.first.size() == 1);
-			Assert::IsTrue(actual.second.size() == 1);
+    EXPECT_TRUE(actual.first.size() == 1);
+    EXPECT_TRUE(actual.second.size() == 1);
 
-			Assert::IsTrue(*index->dataset[actual.first[0]].descriptor == *new float[3]{ 6, 6, 6 });
-		}
+    EXPECT_TRUE(*index->dataset[actual.first[0]].descriptor == (*new float[3]{ 6, 6, 6 }));
+}
 
-		TEST_METHOD(query_given_k_2_returns_closest_points) {
-			Index* index = get_index(1);
-			std::vector<float> q = { 9, 9, 9 };
-			unsigned int k = 2;
-			unsigned int b = 1;
+TEST(ecp_tests, query_given_k_2_returns_closest_points) {
+    Index* index = get_index(1);
+    std::vector<float> q = { 9, 9, 9 };
+    unsigned int k = 2;
+    unsigned int b = 1;
 
-			auto actual = query(index, q, k, b);
+    auto actual = query(index, q, k, b);
 
-			Assert::IsTrue(actual.first.size() == 2);
-			Assert::IsTrue(actual.second.size() == 2);
-		}
-
-#pragma endregion
-	};
+    EXPECT_TRUE(actual.first.size() == 2);
+    EXPECT_TRUE(actual.second.size() == 2);
 }
