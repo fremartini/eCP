b5abb75f21cf17656bb5c558deefa1b58b5e5b32
Morten Skoett <mortenskoett@gmail.com>
2020-10-22 15:02:05 +0200

    Remove sudoing in install_ecp and add comments
    Code integration with ann-benchmarks now works as expected.
    It is necessary to fiddle a bit with docker if images are pulled freshly
    in. A reminder has been added to the script which will require the press
    of enter to continue.

--- scripts/setup_ecp.sh
+++ scripts/setup_ecp.sh
@@ -2,8 +2,9 @@
 
 # Script to install eCP algorithm into ann-benchmarks and build ONLY eCP. It is assumed that 
 # the 'ann-benchmarks' and the 'eCP' repositories are located in the same 
-# dir './' and that this script is called from './eCP.'.
-# It is also assumed that python 3.6 is used eg. using venv.
+# dir './' and that this script is called from './eCP.
+# It is also assumed that python 3.6 is used eg. using venv and that the remote contains the
+# code as the ecp docker image will pull it down inside ann-benchmarks.
 #
 # Example:
 # ./ 
@@ -11,7 +12,12 @@
 #   eCp/
 #     scripts/
 
-echo "Remember to push changes to repo and to start docker daemon" && sleep 3
+echo "Did you remember to
+    1) push changes to remote?
+    2) start docker daemon?
+    3) login to Dockerhub?"
+
+read -p "Press enter to continue"
 
 cd ../..
 
@@ -24,15 +30,16 @@ cp -r eCP/ann-benchmarks/* ann-benchmarks/
 
 echo "Setup python 3.6 env inside ann-benchmarks"
 cd ann-benchmarks
+python3.6 -m venv env
 source env/bin/activate
 
 echo "Install dependencies"
 pip install -r requirements.txt
 
 echo "Building ONLY eCP docker image"
-sudo python install.py --algorithm ecp
+python install.py --algorithm ecp
 
 echo "Run test on random-xs-20-euclidean"
-sudo python run.py --algorithm eCP --dataset random-xs-20-euclidean
+python run.py --algorithm eCP --dataset random-xs-20-euclidean
 sudo python plot.py --dataset random-xs-20-euclidean
 
