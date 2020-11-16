83010c708e3ea565fbb663ae64bf4f464faa4ec9
Morten Skoett <mortenskoett@gmail.com>
2020-11-16 21:52:30 +0100

    Update eCP CMakeLists.txt to handle compiler flags better
    Now both uses max optimization -O3 and -g for debugging symbols. The
    hope was that more vectorization of loops would take place.

--- eCP/app/main.cpp
+++ eCP/app/main.cpp
@@ -6,25 +6,27 @@
 
 int main()
 {
-    /* time ~20 sec */
-    const int L = 2;      //L parameter - number of levels in index
-    const int metric = 0; //Distance metric - 0 = euclidean - 1 = angular
-    const int k = 100;    //# points to return
-    const int b = 40;     //# clusters to search
-    const int p = 200000; //# of vectors
-    const int d = 128;    //dimensions of vector
-    const int r = 10000;  //upper bound of generated vectors
-    const bool debug = false;
+    /* For vtune */
+    const int L = 2;            // L parameter - number of levels in index
+    const int metric = 0;       // Distance metric - 0 = euclidean - 1 = angular
+    const int k = 100;          // number points to return
+    const int b = 40;           // number clusters to search
+    const int p = 150000;       // number of vectors
+    const int d = 128;          // dimensions of vector
+    const int r = 1000;        // upper bound of generated vectors
+    const int queries = 15000;  // queries to make on created index
+    // const bool debug = false;   // print debugging of index   
 
-    /* time ~2 sec */
-    // const int L = 2;    	//L parameter - number of levels in index
-    // const int metric = 0;	//Distance metric - 0 = euclidean - 1 = angular
-    // const int k = 1;		//# points to return
-    // const int b = 1;		//# clusters to search
-    // const int p = 6;	    //# of vectors
-    // const int d = 3;		//dimensions of vector
-    // const int r = 100;		//upper bound of generated vectors
-    // const bool debug = true;
+    /* For debugging */
+    // const int L = 2;            // L parameter - number of levels in index
+    // const int metric = 0;       // Distance metric - 0 = euclidean - 1 = angular
+    // const int k = 5;          // number points to return
+    // const int b = 2;           // number clusters to search
+    // const int p = 20;       // number of vectors
+    // const int d = 5;          // dimensions of vector
+    // const int r = 100;        // upper bound of generated vectors
+    // const int queries = 1;  // queries to make on created index
+    // const bool debug = false;   // print debugging of index   
 
     /* Setup ITTAPI instrumentation domain */
     __itt_domain *domain_build = __itt_domain_create("ECP.BENCHMARKING.BUILD");
@@ -34,7 +36,6 @@ int main()
 
     /* Generate dummy data */
     std::vector<std::vector<float>> S = generate_descriptors(p, d, r);
-    // std::vector<float> q = S[0];
 
     /* Index build instrumentation */
     __itt_task_begin(domain_build, __itt_null, __itt_null, handle_build);
@@ -43,7 +44,7 @@ int main()
 
     /* Query instrumentation */
     __itt_task_begin(domain_query, __itt_null, __itt_null, handle_query);
-    for (int i = 0; i < p / 10; ++i) {
+    for (int i = 0; i < queries; ++i) {
         auto result = query(index, S[i], k, b);
     }
     __itt_task_end(domain_query);
@@ -55,6 +56,7 @@ int main()
     // }
     // print_query_results(result, q, k, index->dataset);
 
+    /* Clean up */
     delete index;
     std::cout << "eCP run done.\n";
     return 0;
--- eCP/scripts/configure.sh
+++ eCP/scripts/configure.sh
@@ -2,4 +2,5 @@
 
 # This script will configure cmake meaning generate make files in build dir
 
-cmake -DCMAKE_BUILD_TYPE=Debug -S ../ -B ../build
+# cmake -DCMAKE_BUILD_TYPE=Debug -S ../ -B ../build
+cmake -S ../ -B ../build
