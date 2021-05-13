86f3bc45f45cb32bceefc5cc4426e08d04e6295c
= <frem@itu.dk>
2021-05-13 13:31:33 +0200

    fix 4

--- eCP/src/eCP/index/shared/distance.cpp
+++ eCP/src/eCP/index/shared/distance.cpp
@@ -26,27 +26,15 @@ inline float euclidean_distance_unroll_halt(const float* a, const float* b, cons
 inline float euclidean_distance_unroll(const float* a, const float* b, const float& threshold = -1)
 {
   globals::DIST_CALCULATIONS++;
-  float sums[] = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
-  for (unsigned int i = 0; i < globals::g_vector_dimensions; i = i + 16) {
+  float sums[] = {0.0, 0.0, 0.0, 0.0};
+  for (unsigned int i = 0; i < globals::g_vector_dimensions; i = i + 4) {
     sums[0] += (a[i] - b[i]) * (a[i] - b[i]);
     sums[1] += (a[i + 1] - b[i + 1]) * (a[i + 1] - b[i + 1]);
     sums[2] += (a[i + 2] - b[i + 2]) * (a[i + 2] - b[i + 2]);
     sums[3] += (a[i + 3] - b[i + 3]) * (a[i + 3] - b[i + 3]);
-    sums[4] += (a[i + 4] - b[i + 4]) * (a[i + 4] - b[i + 4]);
-    sums[5] += (a[i + 5] - b[i + 5]) * (a[i + 5] - b[i + 5]);
-    sums[6] += (a[i + 6] - b[i + 6]) * (a[i + 6] - b[i + 6]);
-    sums[7] += (a[i + 7] - b[i + 7]) * (a[i + 7] - b[i + 7]);
-    sums[8] += (a[i + 8] - b[i + 8]) * (a[i + 8] - b[i + 8]);
-    sums[9] += (a[i + 9] - b[i + 9]) * (a[i + 9] - b[i + 9]);
-    sums[10] += (a[i + 10] - b[i + 10]) * (a[i + 10] - b[i + 10]);
-    sums[11] += (a[i + 11] - b[i + 11]) * (a[i + 11] - b[i + 11]);
-    sums[12] += (a[i + 12] - b[i + 12]) * (a[i + 12] - b[i + 12]);
-    sums[13] += (a[i + 13] - b[i + 13]) * (a[i + 13] - b[i + 13]);
-    sums[14] += (a[i + 14] - b[i + 14]) * (a[i + 14] - b[i + 14]);
-    sums[15] += (a[i + 15] - b[i + 15]) * (a[i + 15] - b[i + 15]);
   }
 
-  return sums[0] + sums[1] + sums[2] + sums[3] + sums[4] + sums[5] + sums[6] + sums[7] + sums[8] + sums[9] + sums[10] + sums[11] + sums[12] + sums[13] + sums[14] + sums[15];
+  return sums[0] + sums[1] + sums[2] + sums[3];
 }
 
 inline float euclidean_distance_halt(const float* a, const float* b, const float& threshold)
